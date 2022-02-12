5bbadcb151fc9010bb45380d13ae72db73854fe2
donghyun-git <donghyun1113@hanmail.net>
2022-02-12 16:13:59 +0900

    2commit

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 
 
 const getLatestNews =()=>{
-  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`);
+  let url = new URL(
+    `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`
+  );
   console.log(url);
 };
 
