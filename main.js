1d650e8b216440c27d0073adaff4601a21e63996
donghyun-git <donghyun1113@hanmail.net>
2022-02-12 16:11:21 +0900

    script

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
 
 const getLatestNews =()=>{
-  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`)
-  console.log(url)
+  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`);
+  console.log(url);
 };
 
-getLatestNews()
+getLatestNews();
