5fb4448878c2a80cec78675fa530071b6dc56af6
donghyun-git <donghyun1113@hanmail.net>
2022-02-14 01:26:55 +0900

    4commit

--- main.js
+++ main.js
@@ -2,14 +2,25 @@
 //2. 헤더준비
 //3. 백엔드 서버에 요청
 //4. 데이터를 보여줌.
+//const callAPI = async() =>{
+  //  let url = new URL(`url주소`)
+    //let header = new Headers({헤더내용}) // 이건 필요한 경우만
+    //let response = await fetch(url,{headers:header})
+    //let data = await response.json()
+//}
 
-const getLatestNews =()=>{
+let news = [];
+const getLatestNews = async()=>{  //비동기 처리, 기존 동기적 언어인 js에서 await을 사용하려면 비동기처리를 선언하는 async를 같이 써주어야 함.
   let url = new URL(
     `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`
   );
   let header = new Headers({'x-api-key':'G4clsHrkRRsNTe19Gs1UhM3XsKLlkg_8LgqJFAmf5bw'})
 
-  let response = fetch(url,{headers:header})
+  let response = await fetch(url,{headers:header}) //ajax, http, fetch
+  let data = await response.json()
+  console.log(response)
+  news = data.articles
+  console.log(news)
 };
 
 getLatestNews();
