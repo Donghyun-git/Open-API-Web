c79ecde384ca051a3b9635f20d3aaf5e0bb0087a
donghyun-git <donghyun1113@hanmail.net>
2022-02-23 22:55:12 +0900

    18commit

--- js/main.js
+++ js/main.js
@@ -15,10 +15,9 @@ let sidemenus = document.querySelectorAll(".side-menu-list button");
 let searchButton = document.getElementById("search-button");
 let keyword = document.getElementById("search-input");
 let url;
-let html = '';
 
 
-menus.forEach((menu)=> menu.addEventListener("click", (event)=>getNewsByTopic(event)));
+menus.forEach((menu) => menu.addEventListener("click", (event) => getNewsByTopic(event)));
 sidemenus.forEach((sidemenu) => sidemenu.addEventListener("click", (event) => getNewsByTopic(event)));
 
 
@@ -46,29 +45,33 @@ const openSearchBox = () => {
 const getNews = async () => {
   try {
     let header = new Headers({
-    'x-api-key': 'G4clsHrkRRsNTe19Gs1UhM3XsKLlkg_8LgqJFAmf5bw'
-  });
+      'x-api-key': 'G4clsHrkRRsNTe19Gs1UhM3XsKLlkg_8LgqJFAmf5bw'
+    });
 
-  let response = await fetch(url, {
-    headers: header
-  }) //ajax, http, fetch
-  let data = await response.json()
-  console.log(response)
-  news = data.articles
-  console.log(news)
-
-  render();
-
-} catch(error){
-  html = `<div class="alert alert-danger" role="alert">
-  뉴스가 존재하지 않습니다!
-  </div>`
-  document.getElementById("news-contents").innerHTML = html;
- }
+    let response = await fetch(url, {
+      headers: header
+    }) //ajax, http, fetch
+    let data = await response.json()
+    console.log(response)
+    console.log(data)
+    if (response.status == 200) {
+      if (data.total_hits == 0) {
+        throw new Error("No match new here!")
+      }
+      news = data.articles
+      console.log(news)
+      render();
+    } else {
+      throw new Error(data.message)
+    }
+  } catch (error) {
+    console.log(error.message);
+    errorRender(error.message);
+  }
 }
 
 const getLatestNews = async () => { //비동기 처리, 기존 동기적 언어인 js에서 await을 사용하려면 비동기처리를 선언하는 async를 같이 써주어야 함.
-   url = new URL(
+  url = new URL(
     `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10`
   );
   getNews();
@@ -80,12 +83,11 @@ getLatestNews();
 const getNewsByTopic = async (event) => {
   console.log("클릭됨", event.target.textContent);
   let topic = event.target.textContent.toLowerCase();
-  url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`
-  );
+  url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`);
   getNews();
 };
 
-const getNewsByKeyword = async ()=>{
+const getNewsByKeyword = async () => {
   //1. 검색 키워드 읽어오기
   //2. url에 검색 키워드 붙히기
   //3. 헤더준비
@@ -94,15 +96,14 @@ const getNewsByKeyword = async ()=>{
   //6. 데이터 보여주기
 
   let keyword = document.getElementById("search-input").value;
-  url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`
-  );
+  url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`);
   getNews();
 }
 
 const render = () => {
   let resultHTML = '';
 
-    resultHTML = news.map((item)=>{
+  resultHTML = news.map((item) => {
 
     return `<div class="row news">
             <div class="col-lg-4">
@@ -120,40 +121,47 @@ const render = () => {
               </div>
             </div>
           </div>`;
-    }).join('');
+  }).join('');
 
-      document.getElementById("news-contents").innerHTML = resultHTML;
+  document.getElementById("news-contents").innerHTML = resultHTML;
 };
 
+const errorRender = (message) => {
+  let errorHTML = '';
+  errorHTML = `<div class="alert alert-danger" role="alert">
+    ${message}
+</div>`
+  document.getElementById("news-contents").innerHTML = errorHTML;
+}
 
 
 searchButton.addEventListener("click", getNewsByKeyword);
-keyword.addEventListener("keyup", (event)=>{
-  if(event.keyCode===13){
+keyword.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
     getNewsByKeyword();
   }
 })
 
-  /*
-  function render() {
-    let resultHTML = '';
-    for (let i = 0; i < news.length; i++) {
-      resultHTML += `<div class="row news">
-              <div class="col-lg-4">
-                <img src=${news[i].media} alt="이미지">
+/*
+function render() {
+  let resultHTML = '';
+  for (let i = 0; i < news.length; i++) {
+    resultHTML += `<div class="row news">
+            <div class="col-lg-4">
+              <img src=${news[i].media} alt="이미지">
+            </div>
+            <div class="col-lg-8">
+              <a href="${news[i].link}"><h2>${news[i].title}</h2></a>
+              <p>
+              ${news[i].summary}
+              </p>
+              <div>
+                <span>출처</span>${news[i].rights} <br>
+                ${news[i].published_date}
               </div>
-              <div class="col-lg-8">
-                <a href="${news[i].link}"><h2>${news[i].title}</h2></a>
-                <p>
-                ${news[i].summary}
-                </p>
-                <div>
-                  <span>출처</span>${news[i].rights} <br>
-                  ${news[i].published_date}
-                </div>
-              </div>
-            </div>`;
-    }
-    document.getElementById("news-contents").innerHTML = resultHTML;
+            </div>
+          </div>`;
   }
-  */
+  document.getElementById("news-contents").innerHTML = resultHTML;
+}
+*/
